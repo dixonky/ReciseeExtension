83c952fcf653c7021533495ff0ef15a5c676e148
Kyle Dixon <46453847+dixonky@users.noreply.github.com>
2020-05-28 09:32:14 -0700

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,16 +1,17 @@
 //Popup Function
 //Author: Kyle Dixon
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('btn');
-  checkPageButton.addEventListener('click', function(tab) {
-    chrome.tabs.executeScript(tab.ib, {
-      file: 'jquery-3.5.1.min.js'
-    });
-    chrome.tabs.executeScript(tab.ib, {
-      file: 'strip.js'  //Get the content on the current page
-    });
-    chrome.tabs.executeScript(tab.ib, {
-      file: 'clean.js'  //serves as a psuedo module as modules are not allowed on content scripts
-    });
-  }, false);
+
+var checkPageButton = document.getElementById('getRecipe');
+checkPageButton.addEventListener("click", function() {
+  chrome.tabs.executeScript({
+    file: 'jquery-3.5.1.min.js'
+  });
+  chrome.tabs.executeScript({
+    file: 'strip.js'  //Get the content on the current page
+  });
+  /*
+  chrome.tabs.executeScript(tab.ib, {
+    file: 'clean.js'  //serves as a psuedo module as modules are not allowed on content scripts
+  });
+  */
 }, false);
